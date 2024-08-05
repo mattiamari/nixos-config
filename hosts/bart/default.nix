4d709add9f0cdb51bd095f15999b3af67eacfee9
Mattia Mari <mattia.mari@proton.me>
2024-08-05 16:19:33 +0200

    Downgrade bart kernel due to nvidia failing compilation

--- hosts/bart/default.nix
+++ hosts/bart/default.nix
@@ -9,7 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   boot.tmp.useTmpfs = true;
 
