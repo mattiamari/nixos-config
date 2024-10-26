c05c1e6bf55d555bc44ccacf2a7be0767aa3202c
Mattia Mari <mattia.mari@proton.me>
2024-10-26 10:21:37 +0200

    Configure jdtls for neovim (doesn't work)

--- home-manager/mattia/nvim/lua/config/lazy.lua
+++ home-manager/mattia/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     -- import/override with your plugins
     { import = "plugins" },
   },
