f5fee16c0611420e520673a96df32ea8edf19522
Mattia Mari <mattia.mari@proton.me>
2025-02-24 08:04:52 +0100

    Fix work neovim config

--- home-manager/work/default.nix
+++ home-manager/work/default.nix
@@ -83,92 +83,102 @@
 
   programs.neovim = {
     enable = true;
+    package = pkgsUnstable.neovim-unwrapped;
     defaultEditor = true;
     vimAlias = true;
+    withPython3 = true;
+
+    plugins = with pkgsUnstable.vimPlugins; [
+      LazyVim
+    ];
 
     extraPackages = with pkgs; [
+      # lazyvim requirements
+      git
+      lazygit
       gcc
       gnumake
-      unzip
+      curl
+      fzf
+      ripgrep
       fd
+
+      unzip
       wl-clipboard
-      #python3 # for jdtls
+      nil # Nix language server
+      jdt-language-server
     ];
-  };
 
-  home.file = {
-    ".config/nvim" = {
-      source = ../mattia/nvim;
-      recursive = true;
-    };
+    extraLuaConfig = ''
+      require("lazy").setup({
+        spec = {
+          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+          { import = "lazyvim.plugins.extras.lang.java" },
 
-    ".config/nvim/lua/plugins/jdtls.lua" = {
-      text = ''
-        return {
+          -- import/override with your plugins
+          -- { import = "plugins" },
+
           {
-            "mfussenegger/nvim-jdtls",
-            opts = function()
-              local mason_registry = require("mason-registry")
-              local lombok_jar = mason_registry.get_package("jdtls"):get_install_path() .. "/lombok.jar"
-              return {
-                -- How to find the root dir for a given filename. The default comes from
-                -- lspconfig which provides a function specifically for java projects.
-                root_dir = require("lspconfig.server_configurations.jdtls").default_config.root_dir,
-
-                -- How to find the project name for a given root dir.
-                project_name = function(root_dir)
-                  return root_dir and vim.fs.basename(root_dir)
-                end,
-
-                -- Where are the config and workspace dirs for a project?
-                jdtls_config_dir = function(project_name)
-                  return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/config"
-                end,
-                jdtls_workspace_dir = function(project_name)
-                  return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/workspace"
-                end,
-
-                -- How to run jdtls. This can be overridden to a full java command-line
-                -- if the Python wrapper script doesn't suffice.
-                cmd = {
-                  "${pkgs.python3}/bin/python",
-                  "${pkgs.jdt-language-server}/bin/jdtls",
-                  string.format("--jvm-arg=-javaagent:%s", lombok_jar),
-                },
-                full_cmd = function(opts)
-                  local fname = vim.api.nvim_buf_get_name(0)
-                  local root_dir = opts.root_dir(fname)
-                  local project_name = opts.project_name(root_dir)
-                  local cmd = vim.deepcopy(opts.cmd)
-                  if project_name then
-                    vim.list_extend(cmd, {
-                      "-configuration",
-                      opts.jdtls_config_dir(project_name),
-                      "-data",
-                      opts.jdtls_workspace_dir(project_name),
-                    })
-                  end
-                  return cmd
-                end,
-
-                -- These depend on nvim-dap, but can additionally be disabled by setting false here.
-                dap = { hotcodereplace = "auto", config_overrides = {} },
-                dap_main = {},
-                test = true,
-                settings = {
-                  java = {
-                    inlayHints = {
-                      parameterNames = {
-                        enabled = "all",
-                      },
-                    },
-                  },
-                },
-              }
-            end
+            "nvim-treesitter/nvim-treesitter",
+            opts = {
+              ensure_installed = {
+                "bash",
+                "html",
+                "css",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "regex",
+                "vim",
+                "yaml",
+                "nix",
+              },
+            },
           },
-        }
-      '';
-      };
+
+          {
+            "neovim/nvim-lspconfig",
+            opts = {
+              servers = {
+                nil_ls = {
+                  mason = false,
+                },
+                jdtls = {
+                  mason = false,
+                },
+              },
+            }
+          }
+
+        },
+        defaults = {
+          lazy = false,
+          version = false, -- always use the latest git commit
+        },
+        install = { colorscheme = { "tokyonight", "habamax" } },
+        checker = {
+          enabled = true, -- check for plugin updates periodically
+          notify = false, -- notify on update
+        }, -- automatically check for plugin updates
+        performance = {
+          rtp = {
+            -- disable some rtp plugins
+            disabled_plugins = {
+              "gzip",
+              -- "matchit",
+              -- "matchparen",
+              -- "netrwPlugin",
+              "tarPlugin",
+              "tohtml",
+              "tutor",
+              "zipPlugin",
+            },
+          },
+        },
+      })
+    '';
   };
 }
