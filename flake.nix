b2a0a3450a72a33efb295c57f66cf4969d831175
Mattia Mari <mattia.mari@proton.me>
2025-06-05 08:17:42 +0200

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -25,68 +25,69 @@
         pkgsMaven = import nixpkgs-maven {
           inherit system;
         };
-      in
-      {
-        nixosConfigurations =
-          let
-            lib = nixpkgs.lib;
-            specialArgs = { inherit meross-prometheus-exporter; };
 
-            defaultModules = [
-              ./hosts/common
+        lib = nixpkgs.lib;
+        specialArgs = { inherit meross-prometheus-exporter; };
+
+        defaultModules = [
+          ./hosts/common
+        ];
+      in {
+        nixosConfigurations = {
+
+          bart = lib.nixosSystem {
+            inherit system specialArgs;
+            modules = defaultModules ++ [
+              {nixpkgs.pkgs = pkgs;}
+              catppuccin.nixosModules.catppuccin
+              ./hosts/bart
+              home-manager.nixosModules.home-manager {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.mattia = import ./home-manager/mattia;
+                home-manager.users.work = import ./home-manager/work;
+                home-manager.extraSpecialArgs = specialArgs // { inherit catppuccin pkgsMaven; };
+              }
             ];
-          in
-          {
+          };
 
-            bart = nixpkgs.lib.nixosSystem {
-              inherit system specialArgs;
-              modules = defaultModules ++ [
-                {nixpkgs.pkgs = pkgs;}
-                catppuccin.nixosModules.catppuccin
-                ./hosts/bart
-                home-manager.nixosModules.home-manager {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.mattia = import ./home-manager/mattia;
-                  home-manager.users.work = import ./home-manager/work;
-                  home-manager.extraSpecialArgs = specialArgs // { inherit catppuccin pkgsMaven; };
-                }
-              ];
-            };
+          homer = lib.nixosSystem {
+            inherit system;
+            specialArgs = specialArgs // {meross-prometheus-exporter = meross-prometheus-exporter.packages.${system}.default;};
+            modules = defaultModules ++ [
+              {nixpkgs.pkgs = pkgs;}
+              ./hosts/homer
+            ];
+          };
 
-            homer = lib.nixosSystem {
-              inherit system;
-              specialArgs = specialArgs // {meross-prometheus-exporter = meross-prometheus-exporter.packages.${system}.default;};
-              modules = defaultModules ++ [ ./hosts/homer ];
-            };
+          marge = lib.nixosSystem {
+            inherit system specialArgs;
+            modules = defaultModules ++ [ ./hosts/marge ];
+          };
 
-            marge = lib.nixosSystem {
-              inherit system specialArgs;
-              modules = defaultModules ++ [ ./hosts/marge ];
-            };
+          wsl = lib.nixosSystem {
+            inherit system specialArgs;
+            modules = defaultModules ++ [
+              nixos-wsl.nixosModules.default
+              catppuccin.nixosModules.catppuccin
+              ./hosts/wsl
+              home-manager.nixosModules.home-manager {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.work = import ./home-manager/work;
+                home-manager.extraSpecialArgs = specialArgs // { inherit catppuccin pkgsMaven; };
+              }
+            ];
+          };
 
-            wsl = lib.nixosSystem {
-              inherit system specialArgs;
-              modules = defaultModules ++ [
-                nixos-wsl.nixosModules.default
-                catppuccin.nixosModules.catppuccin
-                ./hosts/wsl
-                home-manager.nixosModules.home-manager {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.work = import ./home-manager/work;
-                  home-manager.extraSpecialArgs = specialArgs // { inherit catppuccin pkgsMaven; };
-                }
-              ];
-            };
+          rescusb = lib.nixosSystem {
+            inherit system;
+            modules = [
+              "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+              ./hosts/rescusb
+            ];
+          };
 
-            rescusb = lib.nixosSystem {
-              inherit system;
-              modules = [
-                "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-                ./hosts/rescusb
-              ];
-            };
         };
 
         homeConfigurations = {
